06b018f4d2745cb9510cc263ed008cdde6135e03
Jakub Mladý <scrub@students.zcu.cz>
2024-03-09 16:14:54 +0100

    preprocessing single script

--- usage_cs.py
+++
@@ -1,9 +0,0 @@
-from preprocesing_functions_en import nacti_soubor_vypis_spec_znaky, najdi_spec_znaky
-from preprocesing_functions_cs import nahrad_spec_znaky, nahrad_cisla_slovy
-
-cesta = './data/fv1.txt'
-
-#nacti_soubor_vypis_spec_znaky(cesta)
-
-nahrad_cisla_slovy(cesta)
-nahrad_spec_znaky(cesta)
